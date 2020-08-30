2a518faef4ee78643f14de73eb340f2f2a9e5899
Scott Hoisington <hoyzington@gmail.com>
2020-08-30 01:01:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById('time').value;
+  displayMessage(greet(timeString));
 }
 
 /* greet() */
