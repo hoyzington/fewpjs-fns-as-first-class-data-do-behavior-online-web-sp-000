893e51246501fea8b2e012072fb1b773579d62cd
Scott Hoisington <hoyzington@gmail.com>
2020-08-30 02:20:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ function greet(time) {
   const timeNum = time.split(':');
   const hour = parseInt(timeNum[0], 10);
   if (hour < 12) { return 'Good Morning'; }
+  if (hour < 12) { return 'Good Morning'; }
+  if (hour > 12) { return 'Good Morning'; }
 }
 
 function displayMessage(greeting) {
