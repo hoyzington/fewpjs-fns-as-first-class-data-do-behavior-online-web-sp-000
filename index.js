1a97c3bf438690e64cff55471ac479efdf5eed96
Scott Hoisington <hoyzington@gmail.com>
2020-08-30 01:00:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* Given Code, don't edit */
-
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
@@ -7,7 +5,5 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
-/* End Given Code, don't edit above here...*/
-
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+/* greet() */
+/* displayMessage() */
